1d2e90a9a99037f18d9974ac39477876b679a0be
Joshua Frasier <jafrasier50@hotmail.com>
2018-05-03 00:29:19 -0500

    added function to make program end on user input instead of when application finished

--- goodcalc.py
+++ goodcalc.py
@@ -1,34 +1,46 @@
-print("||Four Function Calculator||")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-
-import calcmod
-
-calcmod.addition
-calcmod.subtraction
-calcmod.multiplication
-calcmod.division
-
-choice = None
-
 while True:
-    choice = int(input("|Enter Choice|--> 1 | 2 | 3 | 4 : "))
-    if choice not in [1, 2, 3, 4]:
-        print("Invalid Input. Try Again?")
-    else:
-        break
+    print("----||Four Function Calculator||----")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    import calcmod
+
+    calcmod.addition
+    calcmod.subtraction
+    calcmod.multiplication
+    calcmod.division
+
+    choice = None
+
+    while True:
+        choice = int(input("|Enter Choice|--> 1 | 2 | 3 | 4 : "))
+        if choice not in [1, 2, 3, 4]:
+            print("Invalid Input. Try Again?")
+        else:
+            break
+            continue
+
+    first_number = int(input("----First Number: "))
+    second_number = int(input("----Second Number: "))
+
+    if choice == 1:
+        print(first_number, "+", second_number, "=", first_number + second_number)
+    elif choice == 2:
+        print(first_number, "-", second_number, "=", first_number - second_number)
+    elif choice == 3:
+        print(first_number, "x", second_number, "=", first_number * second_number)
+    elif choice == 4:
+        print(first_number, "/", second_number, "=", first_number / second_number)
+
+    while True:
+        command = str(input("----Run Again? (y/n): "))
+        if command in ("y","n"):
+            break
+        print("----Invalid Input.")
+    if command == "y":
         continue
-
-first_number = int(input("First Number: "))
-second_number = int(input("Second Number: "))
-
-if choice == 1:
-    print(first_number, "+", second_number, "=", first_number + second_number)
-elif choice == 2:
-    print(first_number, "-", second_number, "=", first_number - second_number)
-elif choice == 3:
-    print(first_number, "x", second_number, "=", first_number * second_number)
-elif choice == 4:
-    print(first_number, "/", second_number, "=", first_number / second_number)
+    else:
+        print("  -|| Goodbye ||-   ")
+        break
